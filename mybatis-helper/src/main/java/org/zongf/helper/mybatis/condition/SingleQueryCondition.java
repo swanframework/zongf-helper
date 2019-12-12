5f7a451223dc10ebe1810cd7f267146ec8267963
zonggaofeng <zonggaofeng@autohome.com.cn>
2019-12-12 12:27:01 +0800

    新增查询列名

--- mybatis-helper/src/main/java/org/zongf/helper/mybatis/condition/SingleQueryCondition.java
+++ mybatis-helper/src/main/java/org/zongf/helper/mybatis/condition/SingleQueryCondition.java
@@ -5,6 +5,7 @@ import org.zongf.helper.mybatis.enums.RelationType;
 import org.zongf.helper.mybatis.enums.SortType;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /** 单表查询条件
@@ -12,6 +13,9 @@ import java.util.List;
  * @date: 2019-12-02
  */
 public class SingleQueryCondition {
+
+    // 查询字段名
+    private List<String> fields;
 
     // 条件列表
     private List<ColumnCondition> conditions;
@@ -24,13 +28,18 @@ public class SingleQueryCondition {
         this.orderRules = new ArrayList<>();
     }
 
+    public SingleQueryCondition select(String... fields){
+        this.fields.addAll(Arrays.asList(fields));
+        return this;
+    }
+
     public SingleQueryCondition and(String columnName, OperatorType operatorType, Object value){
         conditions.add(new ColumnCondition(RelationType.AND, columnName, operatorType, value));
         return this;
     }
 
-    public SingleQueryCondition and(String columnName, OperatorType operatorType, Object value, boolean ignoreCase){
-        conditions.add(new ColumnCondition(RelationType.AND, columnName, operatorType, value, ignoreCase));
+    public SingleQueryCondition and(String columnName, OperatorType operatorType, Object value, boolean caseSensitive){
+        conditions.add(new ColumnCondition(RelationType.AND, columnName, operatorType, value, caseSensitive));
         return this;
     }
 
@@ -39,14 +48,22 @@ public class SingleQueryCondition {
         return this;
     }
 
-    public SingleQueryCondition or(String columnName, OperatorType operatorType, Object value, boolean ignoreCase){
-        conditions.add(new ColumnCondition(RelationType.OR, columnName, operatorType, value, ignoreCase));
+    public SingleQueryCondition or(String columnName, OperatorType operatorType, Object value, boolean caseSensitive){
+        conditions.add(new ColumnCondition(RelationType.OR, columnName, operatorType, value, caseSensitive));
         return this;
     }
 
     public SingleQueryCondition orderBy(String columnName, SortType sortType){
         this.orderRules.add(new SortRule(columnName, sortType));
         return this;
+    }
+
+    public List<String> getFields() {
+        return fields;
+    }
+
+    public void setFields(List<String> fields) {
+        this.fields = fields;
     }
 
     public List<ColumnCondition> getConditions() {
